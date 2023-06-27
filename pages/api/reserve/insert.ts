b1a5b5ac88a189d70eb5072744643df29f2201ea
CircuitMFU <skype.2544@gmail.com>
2023-06-27 23:34:22 +0700

    first commit

--- pages/api/reserve/insert.ts
+++ pages/api/reserve/insert.ts
@@ -17,6 +17,7 @@ import mysql from 'mysql2/promise';
 const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
 });
 
