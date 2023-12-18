f30537dfdb2cd6439ee37d35ffb939c2098f077b
CircuitMFU <skype.2544@gmail.com>
2023-12-18 23:54:17 +0700

    first commit

--- pages/admin/backend/booking/buffet/index.tsx
+++ pages/admin/backend/booking/buffet/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Button, Modal } from 'react-bootstrap';
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { Flex } from '@chakra-ui/react';
 import Swal from 'sweetalert2';
-import dynamic from 'next/dynamic';
+// import ColumsLeft from './columsLeft'
+// import ColumsRight from './columsRight'
 import Head from 'next/head';
 
 interface ItemsType {
@@ -42,10 +43,154 @@ interface Buffet {
     shuttle_cock_price: number;
 }
 
-
 function Buffets() {
-    const ColumsLeft = dynamic(() => import("./columsLeft"), { ssr: true });
-    const ColumsRight = dynamic(() => import("./columsRight"), { ssr: true });
+    // const ColumsLeft = dynamic(() => import("./columsLeft"), { ssr: false });
+    // const ColumsRight = dynamic(() => import("./columsRight"), { ssr: false });
+
+    const ColumsLeft = ({ tasks, index, isMobile }: any) => {
+        return (
+            <div style={{ width: '100%' }}>
+                <Droppable droppableId={`left-${index}`} direction="horizontal">
+                    {(droppableProvided) => (
+                        <Flex
+                            maxWidth={"100%"}
+                            height={'100%'}
+                            {...droppableProvided.droppableProps}
+                            ref={droppableProvided.innerRef}
+                        >
+
+                            {tasks.map((task: any, index: number) => (
+                                <>
+                                    <Draggable
+                                        key={task.id}
+                                        draggableId={task.id.toString()}
+                                        index={index}
+                                    >
+                                        {(draggableProvided, draggableSnapshot) => (
+                                            <Flex
+                                                m={"0.2rem"}
+                                                p={"0"}
+                                                width={'150px'}
+                                                maxWidth={"100%"}
+                                                bg={draggableSnapshot.isDragging ? "lightblue" : "white"}
+                                                rounded="3px"
+                                                height={'32px'}
+                                                textAlign="center"
+                                                _active={{ bg: "white" }}
+                                                outline="0px solid transparent"
+                                                outlineColor={
+                                                    draggableSnapshot.isDragging ? "blue" : ""
+                                                }
+                                                boxShadow={
+                                                    draggableSnapshot.isDragging
+                                                        ? "0 5px 10px rgba(0, 0, 0, 0.6)"
+                                                        : "unset"
+                                                }
+                                                align="center"
+                                                flexDirection={"column"}
+                                                zIndex={1}
+                                                {...draggableProvided.dragHandleProps}
+                                                {...draggableProvided.draggableProps}
+                                                ref={draggableProvided.innerRef}
+                                            >
+                                                <span className="p-1 " style={{ fontSize: `${isMobile ? '' : '12px'}` }}>{isMobile ? task.content : task.nickname}</span>
+                                            </Flex>
+                                        )}
+                                    </Draggable>
+                                    {index === 1 && (
+                                        <Flex
+                                            m={"0.2rem"}
+                                            p={"0"}
+                                            width={"50px"}
+                                            height={'32px'}
+
+                                            rounded="3px"
+                                            textAlign="center"
+                                            _active={{ bg: "white" }}
+                                            outline="0px solid transparent"
+
+                                            align="center"
+                                            flexDirection={"column"}
+                                            zIndex={1}
+                                        >
+                                            <span className="p-1 fs-6 text-white fw-bold">VS</span>
+                                        </Flex>
+                                    )}
+                                </>
+                            ))}
+                            {droppableProvided.placeholder}
+
+                        </Flex>
+
+                    )}
+                </Droppable>
+
+            </div>
+
+        );
+    };
+    const ColumsRight = ({ tasks }: any) => {
+        return (
+            <div style={{ width: '100%' }}>
+                <Droppable droppableId={`right`} direction="horizontal" >
+                    {(droppableProvided) => (
+                        <Flex
+                            maxWidth={"100%"}
+                            height={'100%'}
+                            bg={'#0087FF'}
+                            p={"10px"}
+                            rounded={"5px"}
+                            {...droppableProvided.droppableProps}
+                            ref={droppableProvided.innerRef}
+                            css={{
+                                display: "grid",
+                                gridTemplateColumns: "repeat(auto-fill, minmax(150px , 1fr))",
+                                gap: "0.1rem",
+                            }}
+                        >
+                            {tasks.map((task: any, index: any) => (
+                                <Draggable
+                                    key={task.id}
+                                    draggableId={task.id.toString()}
+                                    index={index}
+                                >
+                                    {(draggableProvided, draggableSnapshot) => (
+                                        <Flex
+                                            m={"0.2rem"}
+                                            p={"0"}
+                                            bg={draggableSnapshot.isDragging ? "lightblue" : "white"}
+                                            rounded="3px"
+                                            textAlign="center"
+                                            _active={{ bg: "white" }}
+                                            outline="0px solid transparent"
+                                            outlineColor={
+                                                draggableSnapshot.isDragging ? "blue" : ""
+                                            }
+                                            boxShadow={
+                                                draggableSnapshot.isDragging
+                                                    ? "0 5px 10px rgba(0, 0, 0, 0.6)"
+                                                    : "unset"
+                                            }
+                                            align="center"
+                                            flexDirection={"column"}
+                                            zIndex={1}
+                                            {...draggableProvided.dragHandleProps}
+                                            {...draggableProvided.draggableProps}
+                                            ref={draggableProvided.innerRef}
+                                        >
+                                            <span className="p-1 fs-6">{task.content}</span>
+                                        </Flex>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {droppableProvided.placeholder}
+
+                        </Flex>
+                    )}
+                </Droppable>
+            </div>
+        );
+    };
     const [data, setData] = useState<Buffet[]>([]);
     const [selectDataPayment, setSelectDataPayment] = useState<Buffet | null>();
     const [leftItems, setLeftItems] = useState<ItemsType>(initialLeftItems);
@@ -463,7 +608,7 @@ function Buffets() {
             <div className='mt-2 p-2' style={{ width: '100%', border: "1px solid #5757FF", backgroundColor: "#7A7AF9", borderRadius: '10px', height: 'auto', display: 'flex', gap: "0.1rem", flexDirection: 'row', flexWrap: 'wrap' }}>
                 {data.map((item, index) => (
                     <Flex
-                    key={index}
+                        key={index}
                         m={"0.1rem"}
                         maxWidth={"400px"}
                         rounded="8px"
